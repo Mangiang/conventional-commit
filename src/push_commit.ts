4cfde9e08b440bb9f5baa97336190b96fafd2a59
Mangiang <mangiang@orange.fr>
2020-04-10 16:50:53 -0400

    fix(progress): Fix progress bar not showing while pushing

--- src/push_commit.ts
+++ src/push_commit.ts
@@ -22,17 +22,20 @@ export const push_commit = async () => {
             return text.length === 0 ? 'The branch cannot be empty' : null;
         }
     });
+
     if (remote && branch) {
         useProgressBar(`Pushing ${branch} to ${remote}`, false, new Promise(resolve => {
-            const response: CmdResponse = push(remote, branch);
-            if (response.status !== 0) {
-                if (response.error) { window.showErrorMessage(response.error.message); }
-                if (response.stderr) { window.showErrorMessage(response.stderr.toString('utf-8')); }
-                return;
-            }
-            if (response.stderr) { window.showInformationMessage(response.stderr.toString('utf-8')); }
-            if (response.stdout) { window.showInformationMessage(response.stdout.toString('utf-8')); }
-            resolve();
+            setTimeout(() => {
+                const response: CmdResponse = push(remote, branch);
+                if (response.status !== 0) {
+                    if (response.error) { window.showErrorMessage(response.error.message); }
+                    if (response.stderr) { window.showErrorMessage(response.stderr.toString('utf-8')); }
+                    return;
+                }
+                if (response.stderr) { window.showInformationMessage(response.stderr.toString('utf-8')); }
+                if (response.stdout) { window.showInformationMessage(response.stdout.toString('utf-8')); }
+                resolve();
+            }, 100);
         }));
     }
 };
