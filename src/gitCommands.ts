891020ff7d4bd0038f6bd4202c488284a065c926
Mangiang <mangiang@orange.fr>
2020-04-10 02:09:32 -0400

    feat(push): Add push feature

--- src/gitCommands.ts
+++ src/gitCommands.ts
@@ -1,23 +1,23 @@
-import exec from './execution'
+import exec from './execution';
 
 export const addFiles = (path: string) => {
-    return exec(`git add ${path}`)
-}
+    return exec(`git add ${path}`);
+};
 
 export const commit = (name: string) => {
-    return exec(`git commit -m ${name}`)
-}
+    return exec(`git commit -m ${name}`);
+};
 
 export const getCurrentBranch = () => {
-    const match: RegExpMatchArray | null = exec(`git branch`).match(/^\s*\*\s*(.+)\s*$/)
+    const match: RegExpMatchArray | null = exec(`git branch`).match(/^\s*\*\s*(.+)\s*$/);
 
     if (!match) {
-        return null
+        return null;
     }
     
-    return match[1]
-}
+    return match[1];
+};
 
 export const push = (remote: string, branch: string) => {
-    return exec(`git push ${remote} ${branch}`)
-}
+    return exec(`git push ${remote} ${branch}`);
+};
